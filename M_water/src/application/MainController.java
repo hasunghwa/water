7e9fc9ec1476af9141b1afc0db67f8f285b4e4c4
성화 <@sunghwa>
2019-12-04 02:15:13 +0900

    z

--- M_water/src/application/MainController.java
+++ M_water/src/application/MainController.java
@@ -26,8 +26,6 @@ import javafx.stage.Stage;
 public class MainController {
 	private waterV water = new waterV();
 	private waterDAO dao = new waterDAO();
-	public ObservableList<Integer> countList = FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-			13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25);
 	private ObservableList<waterV> waterList = FXCollections.observableArrayList();
 
 	@FXML
