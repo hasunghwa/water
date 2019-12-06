e650caa50c4ee484d93e8b97f560f65bae0ca7cf
성화 <@sunghwa>
2019-12-06 19:33:28 +0900

    gigi

--- M_water/src/application/Main.java
+++ M_water/src/application/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			System.out.println("");
 			StageStore.stage = primaryStage;
 			Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
 			Scene scene = new Scene(root,799,489);
